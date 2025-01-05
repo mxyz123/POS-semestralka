827264c8ff164670869542751e982c597f76b7ba
mxyz123 <matus.sirotek@gmail.com>
2025-01-06 00:19:35 +0100

    Sockets

--- client.c
+++ client.c
@@ -1,3 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <unistd.h>
+
 int main() {
+    //premmenne
+    int choice, port, status, clientSocket;
+    char ip_addr[19];
+    struct sockaddr_in serv_addr;
+
+    //hra
+    printf("1. Pripojit\n2. Odist\n");
+    scanf("%d", &choice);
+    switch(choice) {
+        case 1:
+            printf("Zadaj adresu ip: ");
+            scanf("%s", ip_addr);
+            printf("Zadaj port: ");
+            scanf("%d", &port);
+            clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+            serv_addr.sin_family = AF_INET;
+            serv_addr.sin_port = htons(port);
+            if (inet_pton(AF_INET, ip_addr, &serv_addr.sin_addr) <= 0) {
+                printf("\nInvalid address/ Address not supported \n");
+                return -1;
+            }
+            status = connect(clientSocket, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+            break;
+        case 2:
+            printf("Exiting.\n");
+            break;
+        default:
+            break;
+    }
+
+    close(clientSocket);
     return 0;
 }
--- server.c
+++ server.c
@@ -1,3 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <unistd.h>
+
 int main() {
+    //premmenne
+    int port, serverSocket, clientSocket, status;
+    struct sockaddr_in serv_addr;
+    struct sockaddr_storage client_addr;
+    socklen_t addr_size;
+    printf("Zadajte port: ");
+    scanf("%d", &port);
+    printf("%d\n");
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    serv_addr.sin_family = AF_INET;
+    inet_pton(AF_INET, "0.0.0.0", &serv_addr.sin_addr);
+    serv_addr.sin_port = htons(port);
+    
+    status = bind(serverSocket, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    listen(serverSocket, 1);
+
+    addr_size = sizeof(client_addr);
+    clientSocket = accept(serverSocket, (struct sockaddr *)&client_addr, &addr_size);
+
+    close(clientSocket);
+    close(serverSocket);
     return 0;
 }
